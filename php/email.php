7926b91b6ca7b565f07c15bd57e86ea152edbaa9
kendrickpeoples <69645658+kendrickpeoples@users.noreply.github.com>
2023-07-11 18:16:22 -0500

    reverted change

--- php/email.php
+++ php/email.php
@@ -29,7 +29,5 @@ $mail->Subject = $subject;
 $mail->Body = $msg;
 
 $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+
+echo 'Message has been sent';
