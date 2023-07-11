626d7d14bbc546e7dfa366f60cea73edd781aa4a
kendrickpeoples <69645658+kendrickpeoples@users.noreply.github.com>
2023-07-11 18:00:01 -0500

    Added PHP to the contact form.

---
+++ php/email.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_POST["name"];
+$subject = $_POST["subject"];
+$email = $_POST["email"];
+$msg = $_POST["msg"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "business@kendrickpeoples.dev";
+$mail->Password = "K3ys3cr3t!!";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("business@kendrickpeoples.dev", "Kendrick");
+
+$mail->Subject = $subject;
+$mail->Body = $msg;
+
+$mail->send();
+
+echo "email sent";
