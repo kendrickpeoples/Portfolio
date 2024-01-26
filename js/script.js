ea0fc777e54e79b030bf712a6a2813dbcd391ad7
kendrickpeoples <69645658+kendrickpeoples@users.noreply.github.com>
2024-01-25 18:37:02 -0600

    added media queries and animations to navbar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+	menu.classList.toggle("bx-x");
+	navbar.classList.toggle("open");
+};
+
+const sr = ScrollReveal({
+	distance: "40px",
+	duration: 2500,
+	reset: true,
+});
+
+sr.reveal(".logo", { delay: 200, origin: "left" });
+sr.reveal(".navbar", { delay: 400, origin: "top" });
+sr.reveal(".menu-btn", { delay: 520, origin: "right" });
